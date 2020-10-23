e51bc415d920f3cd3d161cd54d32affef16fd071
Randy Douglas <randy.e.douglas@gmail.com>
2020-10-23 11:31:03 -0400

    blogful

---
+++ blogful-scripts/seed.blogful.sql
@@ -0,0 +1,12 @@
+
+INSERT INTO blogful_articles (title, content, date_published)
+VALUES
+  ('Brexit’s back: the five issues that will shape science', 'Many researchers might wish they could forget about Brexit. 
+  But they can’t. 
+  The United Kingdom has left the European Union, and for the past ten months it has been in a ‘transition period’ during which little changed. 
+  On 1 January 2021, that will end, bringing changes to public life and policy — including that governing research.
+  UK and EU negotiators are still trying to agree a trade deal that will determine their future relationship, including issues that affect science. 
+  But with only weeks left to make an agreement that can be approved in time, talks have stalled. 
+  The alternative ‘no deal’ scenario is widely predicted to cause chaos, because regulations that control people, goods and services would change abruptly overnight.',
+  '2020-10-21 01:00:00'
+);
